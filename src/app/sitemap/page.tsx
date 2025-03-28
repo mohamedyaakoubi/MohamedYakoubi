7523e66345ce3b46c61696fff757614066851307
mohamedyaakoubi <yakoubimohamed@fss.u-sfax.tn>
2025-03-28 09:11:25 +0100

    sitemap h1

--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function HTMLSitemap() {
     return (
       <div className="container mx-auto p-8">
-        <h2 className="text-3xl font-bold mb-8">Site Map</h2>
+        <h1 className="text-3xl font-bold mb-8">Site Map</h1>
         <ul className="space-y-4">
           <li><a href="/" className="text-blue-600 hover:underline">Home</a></li>
           <li><a href="/projects" className="text-blue-600 hover:underline">Projects</a></li>
